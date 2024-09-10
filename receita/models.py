ee218752d12a6084190f1daf0c2e6105650dcd8d
Alexwallax <alexwallax@gmai.com>
2024-09-10 16:40:46 -0300

    commit 03

--- receita/models.py
+++ receita/models.py
@@ -12,20 +12,17 @@ class Receita(models.Model):
 	ingredientes = models.TextField(blank=True, null=True)
 	modo_preparo = models.TextField(blank=True, null=True)
 	tempo_preparo = models.TimeField(null=True, blank=True)
-	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='Receita')
-	autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Receita')
+	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+	autor = models.ForeignKey(User, on_delete=models.CASCADE)
 	data_criacao = models.DateTimeField(null=True, blank=True)
 	publico_privado = models.BooleanField(default=False)
-
-
-
 
 class Avaliacao(models.Model):
     
     receita = models.ForeignKey(Receita, on_delete=models.CASCADE, related_name='tasks')
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     nota = models.IntegerField(blank=True, null=True)
-    comentarios = models.TextField(blank=False)
+    comentarios = models.TextField(max_length=255, blank=False)
     
 
 		
