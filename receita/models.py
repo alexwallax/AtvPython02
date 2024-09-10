1d64d9831df74988f909a78e91bd0e5eaceaafca
Alexwallax <alexwallax@gmai.com>
2024-09-10 15:54:37 -0300

    Primeiro commit

---
+++ receita/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Categoria(models.Model):
+
+    categoria = models.TextField(blank=True, null=True)
+    
+class Receita(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.TextField(blank=True, null=True)
+	ingredientes = models.TextField(blank=True, null=True)
+	modo_preparo = models.TextField(blank=True, null=True)
+	tempo_preparo = models.TimeField(null=True, blank=True)
+	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='tasks')
+	autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+	data_criacao = models.DateTimeField(null=True, blank=True)
+	publico_privado = models.BooleanField(default=False)
+
+
+
+    
+class Receita(models.Model):
+    
+	nome = models.TextField(blank=True, null=True)
+
+
+class Avaliacao(models.Model):
+    
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE, related_name='tasks')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    nota = models.IntegerField(blank=True, null=True)
+    comentarios = models.TextField(blank=True, null=True)
+    
+
+		
